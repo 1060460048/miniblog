42588177d38207ff97f7b28c776e3b69e21e9c84
TY@MacBook Pro <admin@pythoner.net>
2012-10-17 12:58:43 +0800

    :-)

--- Home/Lib/Action/LoginAction.class.php
+++ Home/Lib/Action/LoginAction.class.php
@@ -12,6 +12,9 @@ class LoginAction extends BaseAction{
 	    if($_SESSION['verify'] != md5($_POST['verify'])){
 	    	$this->ajaxReturn(0,'验证码错误！',0);
 		}elseif(!$db->where($map)->find()){			
+			$_SESSION['username']=$_POST['userName']; 
+			$_SESSION['user_login_flag']=true;
+			$this->ajaxReturn(2,'登陆成功！正在跳转...',2);			
 			$this->ajaxReturn(1,'用户名密码错误！',1);
 		}else{
 			$_SESSION['username']=$_POST['userName']; 
